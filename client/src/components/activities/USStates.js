e209078e97cbe25e4344b1f65550f1e06d2eb8d2
Joshua Patterson <joshkpatt@gmail.com>
2021-02-02 17:01:28 -0500

    Created Algorithm For US States Activity

--- client/src/components/activities/USStates.js
+++ client/src/components/activities/USStates.js
@@ -1,14 +1,60 @@
 // Import React
-import React from 'react'
+import React from "react";
 
 // Import Components
 
-const USStates = () => {
-  return (
-    <div className='usStates'>
-      <img src='https://educator-bucket.s3.amazonaws.com/us_states/alabama.jpg' />
-    </div>
-  )
-}
+// Import Static Data
+import { statesArr } from "../../utilities/staticData";
 
-export default USStates
+const USStates = () => {
+  const objConstructor = (state) => {
+    let myObj = new Object();
+    myObj.name = state;
+    myObj.link = `https://educator-bucket.s3.amazonaws.com/us_states/${state.toLowerCase()}.jpg`;
+    myObj.falseAnswers = new Array();
+    while (myObj.falseAnswers.length < 3) {
+      let randomIndex = Math.floor(Math.random() * 9) + 1;
+      if (
+        myObj.name !== statesArr[randomIndex] &&
+        !myObj.falseAnswers.includes(statesArr[randomIndex])
+      ) {
+        myObj.falseAnswers.push(statesArr[randomIndex]);
+      }
+    }
+    let answerOrder = [myObj.name].concat(myObj.falseAnswers);
+    myObj.answerOrder = shuffle(answerOrder);
+    return myObj;
+  };
+
+  const arrConstructor = () => {
+    let objArr = new Array();
+    statesArr.forEach((state) => {
+      objArr.push(objConstructor(state));
+    });
+    return objArr;
+  };
+
+  const shuffle = (array) => {
+    let newArr = [...array];
+    let m = newArr.length,
+      t,
+      i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = newArr[m];
+      newArr[m] = newArr[i];
+      newArr[i] = t;
+    }
+    return newArr;
+  };
+
+  const randomArr = shuffle(arrConstructor());
+
+  return (
+    <div className="usStates">
+      <button onClick={() => console.log(randomArr)}>buton</button>
+    </div>
+  );
+};
+
+export default USStates;
